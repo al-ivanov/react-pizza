fe6fa5308f738bd94d708e6c199a0c58ef7177d2
Alexandr ivanov <frontend-master@twinscorp.ru>
2022-01-17 18:01:31 +0300

    Работа с фейковым сервером

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
@@ -8,12 +9,19 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes >
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home items={pizzas} />} exact />
           <Route path="/cart" element={<Cart />} exact />
         </Routes >
       </div>
